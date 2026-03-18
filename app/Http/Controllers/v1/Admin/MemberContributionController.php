07992d6c225444f1448b90ad450136fe63241279
Fortune4808 <opeyemiogunleye49@gmail.com>
2026-03-18 21:14:47 +0100

    I added the deposi wallet fucntion

--- app/Http/Controllers/v1/Admin/MemberContributionController.php
+++ app/Http/Controllers/v1/Admin/MemberContributionController.php
@@ -3,11 +3,17 @@
 namespace App\Http\Controllers\v1\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Models\Admin\MemberContribution;
+use App\Models\Admin\MemberContributionSaving;
+use App\Models\User\User;
+use App\Services\Finance\WalletService;
+use Illuminate\Database\UniqueConstraintViolationException;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class MemberContributionController extends Controller
 {
-   // Display a listing of the resource.
+    // Display a listing of the resource.
     public function index()
     {
         //
@@ -16,7 +22,42 @@ class MemberContributionController extends Controller
     // Store a newly created resource in storage.
     public function store(Request $request)
     {
-        //
+        try {
+
+            return DB::transaction(function () use ($request) {
+
+                $userId = $request->header('X-User-ID');
+
+                $contributionAmount = MemberContributionSaving::where('user_id', $userId)
+                    ->value('contribution_amount');
+
+                $user = User::where('user_id', $userId)->first();
+
+                WalletService::deposit(
+                    $userId,
+                    $contributionAmount,
+                    null,
+                    'Monthly contribution deposit for user ' . $user->first_name . ' ' . $user->last_name . ' for ' . now()->format('F Y')
+                );
+
+                MemberContribution::create([
+                    'user_id' => $userId,
+                    'contribution_amount' => $contributionAmount,
+                    'contribution_date' => now(),
+                    'status_id' => 21,
+                ]);
+
+                return response()->json([
+                    'success' => true,
+                    'message' => 'Contribution processed successfully'
+                ], 201);
+            });
+        } catch (\Illuminate\Database\UniqueConstraintViolationException $e) {
+            return response()->json([
+                'success' => false,
+                'message' => 'You have already made a contribution for this month. Payment cannot be made twice.'
+            ], 409);
+        }
     }
 
     // Display the specified resource.
