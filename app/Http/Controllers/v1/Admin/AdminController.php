c6f85537befc25122cc49aab30ba796828d0dbc3
ADEYEMI AYOBAMI SAMSON <adeyemiayobami273@gmail.com>
2026-04-10 12:57:52 +0100

    modified admin controller and user managemnet controller

--- app/Http/Controllers/v1/Admin/AdminController.php
+++ app/Http/Controllers/v1/Admin/AdminController.php
@@ -2,20 +2,22 @@
 
 namespace App\Http\Controllers\v1\Admin;
 
-use App\Services\Config;
-use App\Models\Admin\Staff;
-use Illuminate\Support\Arr;
-use Illuminate\Support\Str;
-use App\Jobs\ActivityLogJob;
-use Illuminate\Http\Request;
-use App\Models\Setup\SetupCounter;
-use Spatie\Permission\Models\Role;
 use App\Http\Controllers\Controller;
+use App\Http\Resources\Admin\AdminResource;
+use App\Jobs\ActivityLogJob;
+use App\Models\Admin\ActivityLog;
+use App\Models\Admin\Staff;
+use App\Models\Setup\SetupCounter;
+use App\Notifications\Admin\StaffRegistration;
+use App\Services\Cache\ClearCacheService;
+use App\Services\Config;
+use Illuminate\Http\Request;
+use Illuminate\Support\Arr;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Cache;
-use App\Services\Cache\ClearCacheService;
-use App\Http\Resources\Admin\AdminResource;
-use App\Notifications\Admin\StaffRegistration;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Str;
+use Spatie\Permission\Models\Role;
 use Symfony\Component\HttpFoundation\JsonResponse;
 
 class AdminController extends Controller
@@ -82,57 +84,62 @@ class AdminController extends Controller
             'roleId' => 'required|int|exists:roles,id'
         ]);
 
-        $admin = Auth::guard('admin')->user();
-        $staffId = SetupCounter::generateCustomId('STFF');
-        $staff = Staff::create([
-            'staff_id'      => $staffId,
-            'title_id'      => $request->titleId,
-            'first_name'    => strtoupper($request->firstName),
-            'middle_name'   => strtoupper($request->middleName),
-            'last_name'     => strtoupper($request->lastName),
-            'gender_id'     => $request->genderId,
-            'email'         => strtolower($request->emailAddress),
-            'mobile_number' => $request->mobileNumber,
-            'home_address'  => strtoupper($request->homeAddress),
-            'created_by'    => $admin->staff_id ?? null,
-            'password'      => $staffId,
-        ]);
-        $role = Role::findById($request->roleId, 'admin');
-        $staff->assignRole($role);
-        ClearCacheService::clearListCache('staff_list');
-        $fullName = $staff->first_name . ' ' . ($staff->middle_name ? $staff->middle_name . ' ' : '') . $staff->last_name;
-        $titleName= Config::getTitleNameById($request->titleId);
-        $staff->notify(new StaffRegistration(Str::title($fullName), $staffId, Str::title($titleName)));
+        return DB::transaction(function () use ($request) {
 
-        // $registeredData = $staff->only([
-        //     'staff_id',
-        //     'title_id',
-        //     'first_name',
-        //     'middle_name',
-        //     'last_name',
-        //     'gender_id',
-        //     'email',
-        //     'mobile_number',
-        //     'status',
-        //     'created_by',
-        //     'created_at',
-        // ]);
-        // $registeredData['role'] = $role->name;
-        // ActivityLogJob::dispatch(
-        //     action: 'New staff registration',
-        //     description: 'A new staff with ID: ' . $staffId . ' and ' . $role->name .  ' role has been registered to the system.',
-        //     userType: 'Central Staff',
-        //     performedBy: auth('admin')->id() ?? $staffId,
-        //     roleId: auth('admin')->user()?->roles?->pluck('id')->first() ?? null,
-        //     metadata: [
-        //         'Registered Data' => $registeredData,
-        //     ]
-        // );
+            $admin = Auth::guard('admin')->user();
+            $staffId = SetupCounter::generateCustomId('STFF');
+            $staff = Staff::create([
+                'staff_id'      => $staffId,
+                'title_id'      => $request->titleId,
+                'first_name'    => strtoupper($request->firstName),
+                'middle_name'   => strtoupper($request->middleName),
+                'last_name'     => strtoupper($request->lastName),
+                'gender_id'     => $request->genderId,
+                'email'         => strtolower($request->emailAddress),
+                'mobile_number' => $request->mobileNumber,
+                'home_address'  => strtoupper($request->homeAddress),
+                'created_by'    => $admin->staff_id ?? null,
+                'password'      => $staffId,
+            ]);
+            $role = Role::findById($request->roleId, 'admin');
+            $staff->assignRole($role);
+            ClearCacheService::clearListCache('staff_list');
+            $fullName = $staff->first_name . ' ' . ($staff->middle_name ? $staff->middle_name . ' ' : '') . $staff->last_name;
+            $titleName = Config::getTitleNameById($request->titleId);
+            $staff->notify(new StaffRegistration(Str::title($fullName), $staffId, Str::title($titleName)));
 
-        return response()->json([
-            'success'  => true,
-            'message' => 'Staff created successfully',
-        ], 201);
+            $registeredData = $staff->only([
+                'staff_id',
+                'title_id',
+                'first_name',
+                'middle_name',
+                'last_name',
+                'gender_id',
+                'email',
+                'mobile_number',
+                'status',
+                'created_by',
+                'created_at',
+            ]);
+            $registeredData['role'] = $role->name;
+            ActivityLogJob::dispatch(
+                modelClass: ActivityLog::class,
+                action: 'New staff registration',
+                description: 'A new staff with ID: ' . $staffId . ' and ' . $role->name .  ' role has been registered to the system.',
+                userType: 'Staff',
+                performedBy: auth('admin')->id() ?? $staffId,
+                roleId: auth('admin')->user()?->roles?->pluck('id')->first() ?? 0,
+                metadata: [
+                    'Registered Data' => $registeredData,
+                ],
+                deviceInfo: Config::requestDetails(),
+            );
+
+            return response()->json([
+                'success'  => true,
+                'message' => 'Staff created successfully',
+            ], 201);
+        });
     }
 
     // Display the specified resource.
@@ -167,21 +174,6 @@ class AdminController extends Controller
     public function update(Request $request, string $id)
     {
         $updateAdmin = Staff::with(['roles'])->findOrFail($id);
-        $dataBeforeUpdate = Arr::only($updateAdmin->getOriginal(), [
-            'title_id',
-            'first_name',
-            'middle_name',
-            'last_name',
-            'gender_id',
-            'email',
-            'mobile_number',
-            'status_id',
-            'lga_id',
-            'date_of_birth',
-            'nin',
-            'home_address'
-        ]);
-        $dataBeforeUpdate['role'] = $updateAdmin->roles->pluck('name')->toArray();
         $request->validate([
             'titleId' => 'required|integer|exists:setup_titles,title_id',
             'firstName' => ['required', 'string', 'regex:/^[A-Za-z\s\'-]+$/', 'min:2', 'max:50'],
@@ -197,7 +189,24 @@ class AdminController extends Controller
             'statusId' => 'required|integer|exists:setup_statuses,status_id',
         ]);
 
+
         try {
+            $dataBeforeUpdate['role'] = $updateAdmin->roles->pluck('name')->toArray();
+            $dataBeforeUpdate = Arr::only($updateAdmin->getOriginal(), [
+                'title_id',
+                'first_name',
+                'middle_name',
+                'last_name',
+                'gender_id',
+                'email',
+                'mobile_number',
+                'status_id',
+                'lga_id',
+                'date_of_birth',
+                'nin',
+                'home_address'
+            ]);
+
             $admin = Auth::guard('admin')->user();
             $updateAdmin->update([
                 'title_id'       => $request->titleId,
@@ -226,19 +235,26 @@ class AdminController extends Controller
                 'gender_id',
                 'email',
                 'mobile_number',
+                'home_address',
+                'lga_id',
+                'date_of_birth',
+                'nin',
                 'status_id',
+                'updated_by',
             ]);
             $dataAfterUpdate['role'] = $updateAdmin->roles->pluck('name')->toArray();
             ActivityLogJob::dispatch(
+                modelClass: ActivityLog::class,
                 action: 'Staff update',
                 description: 'Staff with ID: ' . $id . ' has been updated.',
                 userType: 'Admin',
                 performedBy: auth('admin')->id() ?? "System",
-                roleId: auth('admin')->user()?->roles?->pluck('id')->first() ?? null,
+                roleId: auth('admin')->user()?->roles?->pluck('id')->first() ?? 0,
                 metadata: [
                     'Before Update' => $dataBeforeUpdate,
                     'After Update' => $dataAfterUpdate,
-                ]
+                ],
+                deviceInfo: Config::requestDetails(),
             );
 
             return response()->json([
--- routes/api.php
+++ routes/api.php
@@ -16,12 +16,12 @@ use App\Http\Controllers\v1\Setup\GenderController;
 use App\Http\Controllers\v1\Setup\LgaController;
 use App\Http\Controllers\v1\Setup\MembershipTypeController;
 use App\Http\Controllers\v1\Setup\PaymentChannelTypeController;
+use App\Http\Controllers\v1\Setup\StaffCategoryController;
 use App\Http\Controllers\v1\Setup\StateController;
 use App\Http\Controllers\v1\Setup\StatusController;
 use App\Http\Controllers\v1\Setup\TitleController;
 use App\Http\Controllers\v1\User\Auth\UserAuthController;
 use App\Http\Controllers\v1\User\UserPassportController;
-use App\Models\Setup\StaffCategory;
 use Illuminate\Support\Facades\Route;
 
 
@@ -36,8 +36,8 @@ Route::prefix('v1')->group(function () {
         });
 
         Route::middleware(['auth:admin', 'trust.device'])->group(function () {
-            // Route::apiResource('role', RoleController::class)->middleware('permission:manage roles');
-            // Route::apiResource('staff', AdminController::class)->middleware('permission:manage staff');
+            Route::apiResource('role', RoleController::class)->middleware('permission:manage roles');
+            Route::apiResource('staff', AdminController::class)->middleware('permission:manage staff');
             Route::post('change-password', [AdminAuthController::class, 'changePassword'])->middleware('throttle:5,1');
             Route::apiResource('users', UserManagementController::class)->middleware('permission:manage users');
             Route::post('staff-passport/{id}', [StaffPassportController::class, 'update']);
@@ -100,6 +100,6 @@ Route::prefix('v1')->group(function () {
         Route::get('status', [StatusController::class, 'index']);
         Route::get('payment-channel-types', [PaymentChannelTypeController::class, 'index']);
         Route::get('membership-types', [MembershipTypeController::class, 'index']);
-        Route::get('staff-category', [StaffCategory::class, 'index']);
+        Route::get('staff-category', [StaffCategoryController::class, 'index']);
     });
 });
