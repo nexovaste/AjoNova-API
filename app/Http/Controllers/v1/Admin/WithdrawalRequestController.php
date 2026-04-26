4792f607825a0d5afb29b2c6f7fb6228b852bdaa
Fortune4808 <opeyemiogunleye49@gmail.com>
2026-04-26 09:50:36 +0100

    Added new endpoints to my workspace

--- app/Http/Controllers/v1/Admin/WithdrawalRequestController.php
+++ app/Http/Controllers/v1/Admin/WithdrawalRequestController.php
@@ -3,24 +3,77 @@
 namespace App\Http\Controllers\v1\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Http\Resources\Admin\WithdrawalRequestResource;
+use App\Models\Admin\WithdrawalRequest;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Cache;
 
 
-class WalletController extends Controller
+class WithdrawalRequestController extends Controller
 {
     // Display a listing of the resource.
-    public function index()
+    public function index(Request $request)
     {
-        //
+        try {
+            $cursor = $request->query('cursor');
+            $cacheKey = "withdrawal_request_list_" . ($cursor ?? 'first_page');
+            $withdrawalRequestData = Cache::tags('withdrawal_request_list')->flexible(
+                $cacheKey,
+                [now()->addMonth(), null],
+                function () use ($cursor) {
+                    return WithdrawalRequest::with([
+                        'status:status_id,status_name',
+                    ])->cursorPaginate(30, ['*'], 'cursor', $cursor);
+                }
+            );
+
+            if ($withdrawalRequestData->isEmpty()) {
+                return response()->json([
+                    'success' => false,
+                    'message' => 'No withdrawal requests found.',
+                    'data' => []
+                ], 404);
+            }
+
+            return response()->json([
+                'success' => true,
+                'message' => 'Withdrawal requests fetched successfully.',
+                'data' => WithdrawalRequestResource::collection($withdrawalRequestData),
+                'pagination' => [
+                    'next_cursor' => $withdrawalRequestData->nextCursor()?->encode(),
+                    'previous_cursor' => $withdrawalRequestData->previousCursor()?->encode(),
+                ],
+            ], 200);
+        } catch (\Exception $e) {
+            return response()->json([
+                'success' => false,
+                'message' => $e->getMessage()
+            ], 400);
+        }
     }
 
 
     // Display the specified resource.
     public function show(string $id)
     {
-        //
+
+        try {
+            $withdrawalRequest = Cache::remember("withdrawal_request_{$id}", now()->addMonth(), function () use ($id) {
+                return new WithdrawalRequestResource(WithdrawalRequest::with([
+                    'status:status_id,status_name'
+                ])->findOrFail($id));
+            });
+
+            return response()->json([
+                'success' => true,
+                'message' => 'Withdrawal request fetched successfully.',
+                'data' => $withdrawalRequest
+            ], 200);
+        } catch (\Exception $e) {
+            return response()->json([
+                'success' => false,
+                'message' => 'Failed to retrieve withdrawal request: ' . $e->getMessage()
+            ], 500);
+        }
     }
-
-   
-
-  
 }
